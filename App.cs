efcbfed6da4d520743f9b1ca69dedcf40f642d5d
Falko Schindler <mail@falkoschindler.de>
2014-07-11 16:00:15 +0200

    further simplified implementation

--- App.cs
+++ App.cs
@@ -6,56 +6,30 @@ namespace FormsMenuIcon
 {
     public class App
     {
+        static MasterDetailPage MDPage;
+
         public static Page GetMainPage()
-        {   
-            var page = new MasterDetailPage();
-            page.Master = new MasterPage(page);
-            page.Detail = new DetailPage("A");
-            page.IsPresentedChanged += delegate {
-                page.Title = page.IsPresented ? page.Master.Title : page.Detail.Title;
-            };
-            page.Appearing += async delegate {
-                await Task.Delay(0);
-                page.IsPresented = true;
-            };
-            page.ToolbarItems.Add(new ToolbarItem("Hi!", null, delegate {
-                Console.WriteLine("Hi!");
-            }));
-            return new NavigationPage(page);
-        }
-    }
-
-    public class MasterPage: ContentPage
-    {
-        MasterDetailPage masterDetailPage;
-
-        public MasterPage(MasterDetailPage masterDetail)
         {
-            Title = "Master";
-            Content = new StackLayout {
-                Children = { Link("A"), Link("B"), Link("C") }
-            };
-            this.masterDetailPage = masterDetail;
+            return new NavigationPage(
+                MDPage = new MasterDetailPage {
+                    Master = new ContentPage {
+                        Title = "Master",
+                        Content = new StackLayout {
+                            Children = { Link("A"), Link("B"), Link("C") }
+                        },
+                    },
+                    Detail = new ContentPage { Content = new Label { Text = "A" } },
+                });
         }
 
-        Button Link(string name)
+        static Button Link(string name)
         {
             var button = new Button { Text = name };
             button.Clicked += delegate {
-                masterDetailPage.Detail = new DetailPage(name);
-                masterDetailPage.IsPresented = false;
+                MDPage.Detail = new ContentPage { Content = new Label { Text = name } };
+                MDPage.IsPresented = false;
             };
             return button;
         }
     }
-
-    public class DetailPage: ContentPage
-    {
-        public DetailPage(string name)
-        {
-            Title = name;
-            Content = new Label { Text = name };
-        }
-    }
 }
-
--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -9,22 +9,22 @@ using Xamarin.Forms;
 
 namespace FormsMenuIcon.iOS
 {
-	[Register ("AppDelegate")]
-	public partial class AppDelegate : UIApplicationDelegate
-	{
-		UIWindow window;
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        UIWindow window;
 
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
-		{
-			Forms.Init ();
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Forms.Init();
 
-			window = new UIWindow (UIScreen.MainScreen.Bounds);
+            window = new UIWindow(UIScreen.MainScreen.Bounds);
 			
-			window.RootViewController = App.GetMainPage ().CreateViewController ();
-			window.MakeKeyAndVisible ();
+            window.RootViewController = App.GetMainPage().CreateViewController();
+            window.MakeKeyAndVisible();
 			
-			return true;
-		}
-	}
+            return true;
+        }
+    }
 }
 
