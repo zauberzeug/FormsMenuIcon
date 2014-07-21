48b82de57f2bccea553d9edf095297d1a8f51a09
Falko Schindler <mail@falkoschindler.de>
2014-07-21 06:47:05 +0200

    trying to implement Chase Florell's solution

--- App.cs
+++ App.cs
@@ -1,35 +1,58 @@
-﻿using System;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace FormsMenuIcon
 {
-    public class App
+    public static class App
     {
-        static MasterDetailPage MDPage;
+        public static INavigation Navigation { get; set; }
 
         public static Page GetMainPage()
         {
-            return new NavigationPage(
-                MDPage = new MasterDetailPage {
-                    Master = new ContentPage {
-                        Title = "Master",
-                        Content = new StackLayout {
-                            Children = { Link("A"), Link("B"), Link("C") }
-                        },
-                    },
-                    Detail = new ContentPage { Content = new Label { Text = "A" } },
-                });
+            return new MainPage();
+        }
+    }
+
+    public class MainPage : MasterDetailPage
+    {
+        
+        public MainPage()
+        {
+            Title = "Some Title";
+            var master = new MainMenu();
+            var detail = new NavigationPage(new FirstPage("FirstPage"));
+            
+            App.Navigation = App.Navigation ?? detail.Navigation;
+            
+            Master = master;
+            Detail = detail;
+        }
+    }
+
+    public class MainMenu: ContentPage
+    {
+        public MainMenu()
+        {
+            Title = "MainMenu";
+            Content = new StackLayout {
+                Children = { Link("A"), Link("B"), Link("C") }
+            };
         }
 
         static Button Link(string name)
         {
             var button = new Button { Text = name };
-            button.Clicked += delegate {
-                MDPage.Detail = new ContentPage { Content = new Label { Text = name } };
-                MDPage.IsPresented = false;
+            button.Clicked += async delegate {
+                await App.Navigation.PushAsync(new FirstPage(name));
             };
             return button;
+        }
+    }
+
+    public class FirstPage: ContentPage
+    {
+        public FirstPage(string text)
+        {
+            Content = new Label{ Text = text };
         }
     }
 }
