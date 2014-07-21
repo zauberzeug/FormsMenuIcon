c0c7fd029881035c0f62dd72439e92c22bbe45d9
Rodja Trappe <mail@rodja.net>
2014-07-21 15:41:39 +0200

    trying new renderer

---
+++ iOS/NavigationPageRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace FormsMenuIcon.iOS
+{
+    [assembly:ExportRenderer(typeof(NavigationPage), typeof(MyNavigationPageRenderer))]
+
+    public class MyNavigationPageRenderer : NavigationRenderer
+    {
+        public override void ViewWillAppear(bool animated)
+        {
+            NavigationBar.BackItem.Title = "M";
+            base.ViewWillAppear(animated);
+        }
+    }
+}
+
