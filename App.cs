7329cb59e3eb2564e7e6306eddd1f414c24f93ef
Falko Schindler <mail@falkoschindler.de>
2014-10-10 14:26:16 +0200

    http://stackoverflow.com/q/26299327/3419103

--- App.cs
+++ App.cs
@@ -13,38 +13,18 @@ namespace FormsMenuIcon
                 Master = new ContentPage {
                     Title = "Master",
                     Icon = Device.OS == TargetPlatform.iOS ? "menu.png" : null,
-                    Content = new StackLayout {
-                        Children = { MenuLink("A"), MenuLink("B"), MenuLink("C") }
+                    Content = new Button {
+                        Text = "Open detail",
+                        Command = new Command(o => {
+                            MDPage.Detail = new NavigationPage(new ContentPage());
+                            MDPage.IsPresented = false;
+                        }),
                     },
                 },
-                Detail = new NavigationPage(CreateContentPage("A")),
+                Detail = new NavigationPage(new ContentPage()),
             };
             MDPage.IsPresentedChanged += (sender, e) => Console.WriteLine(DateTime.Now + ": " + MDPage.IsPresented);
             return MDPage;
-        }
-
-        static Button MenuLink(string name)
-        {
-            return new Button {
-                Text = name,
-                Command = new Command(o => {
-                    MDPage.Detail = new NavigationPage(CreateContentPage(name));
-                    MDPage.IsPresented = false;
-                }),
-            };
-        }
-
-        static Button Link(string name)
-        {
-            return new Button {
-                Text = name,
-                Command = new Command(o => MDPage.Detail.Navigation.PushAsync(CreateContentPage(name))),
-            };
-        }
-
-        static ContentPage CreateContentPage(string text)
-        {
-            return new ContentPage { Title = text, Content = Link(text + ".sub") };
         }
     }
 }
